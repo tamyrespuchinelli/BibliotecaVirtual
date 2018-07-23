b705bfd2df934116067b4695229d59352346a7c6
tamyrespuchinelli <tamyrespuchinelli@hotmail.com>
2018-07-23 17:48:07 -0300

    Login e index
    Criação do Login e index

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,7 @@
 <template >
-  <div>
-     <b-navbar id="nav" toggleable type="dark"  sticky>
-       <b-container>
-           <b-navbar-brand >Biblioteca Digital</b-navbar-brand>
-
-
-
-    <b-form inline id="form">
-      <label class="name" for="name"></label>
-      <b-input class="nome" id="nome" placeholder="nome" />&ensp;
-      <label class="senha" for="senha"> </label>
-      <b-input-group  class="senha">
-        <b-input id="senha" placeholder="senha" />
-      </b-input-group>&ensp;
-      <b-form-checkbox class="mb-2 mr-sm-2 mb-sm-0">Remember me</b-form-checkbox>
-      <b-button id="button" >Entrar</b-button>
-    </b-form>
-
-     
-           <b-navbar-toggle target="nav_text_collapse"></b-navbar-toggle>
-       </b-container>
-   </b-navbar>
-
-<b-container>
-<img id="logo" src=".\assets\logo.png" width="500"  >
-   
-</b-container>
-  </div>
+  <div id="app">
+<router-view/>
+</div>
 </template>
 
 <script>
@@ -34,22 +9,5 @@
 </script>
 
 <style>
-#nav{
-    background-color:dimgray;
-}
-#form{
-    font-style: normal;
-    color:ghostwhite;
-}
-#button{
-    color:lightpink;
-    background-color: white;
-}
-#logo{
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-             
-  
-}
+
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,73 @@
+<template>
+
+<div> 
+<b-navbar toggleable="md" type="dark" variant="dark" >
+<b-navbar-brand href="#">Biblioteca Virtual</b-navbar-brand>
+
+</b-navbar>
+
+<b-container class ="container">
+	<b-container class="wrapper">
+		<b-form action="" method="post" name="Login_Form" class="form-signin">       
+		    <h3 class="form-signin-heading">Login</h3>
+			  
+			  
+			  <b-input type="text" class="form-control" name="Username" placeholder="Nome" required="" autofocus="" />
+			  <b-input type="password" class="form-control" name="Password" placeholder="Senha" required=""/>     		  
+			 
+			  <b-button class="btn btn-lg btn-primary btn-block"  name="Submit" value="Login" type="Submit">Entrar</b-button>  			
+		</b-form>			
+	</b-container>
+</b-container>
+   
+  </div>
+
+ 
+</template>
+
+<script>
+
+</script>
+
+<style>
+        
+        .wrapper {    
+	margin-top: 180px;
+	margin-bottom: 20px;
+}
+
+.form-signin {
+  max-width: 420px;
+  padding: 30px 38px 66px;
+  margin: 0 auto;
+  background-color: #eee;
+  border: 3px dotted rgba(0,0,0,0.1);  
+  }
+
+.form-signin-heading {
+  text-align:center;
+  margin-bottom: 30px;
+}
+
+.form-control {
+  position: relative;
+  font-size: 16px;
+  height: auto;
+  padding: 10px;
+}
+
+input[type="text"] {
+  margin-bottom: 0px;
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+}
+
+input[type="password"] {
+  margin-bottom: 20px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+
+
+        
+</style>
