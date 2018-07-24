32a2c0860fd5ba746bf88729868c0d41f6d90bca
tamyrespuchinelli <tamyrespuchinelli@hotmail.com>
2018-07-23 22:54:24 -0300

    criação de livros

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -9,8 +9,8 @@
 
 
 <div>
-  <b-card title="Card Title"
-          img-src="https://picsum.photos/600/300/?image=25"
+  <b-card title="Harry Potter e a Pedra Filosofal"
+          img-src="https://static.quizur.com/i/b/587544312cec14.72202178Capa-Harry-Potter-e-a-Pedra-Filosofal-%28livro%29.jpg"
           img-alt="Image"
           img-top
           tag="article"
@@ -19,7 +19,23 @@
     <p class="card-text">
       Some quick example text to build on the card title and make up the bulk of the card's content.
     </p>
-    <b-button href="#" variant="primary">Go somewhere</b-button>
+    <b-button href="#" variant="primary">Ler mais...</b-button>
+  </b-card>
+</div>
+
+
+<div>
+  <b-card title="Como eu era antes de você"
+          img-src="https://images.livrariasaraiva.com.br/imagemnet/imagem.aspx/?pro_id=4889143&qld=90&l=430&a=-1"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 20rem;"
+          class="mb-2">
+    <p class="card-text">
+      Some quick example text to build on the card title and make up the bulk of the card's content.
+    </p>
+    <b-button href="#" variant="primary">Ler mais...</b-button>
   </b-card>
 </div>
 </div>
